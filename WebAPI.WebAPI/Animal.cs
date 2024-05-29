aac70179b7a580e978c6ec87c0d6995499ad9368
DESKTOP-FISKR1J\JanHP <jannemetguzvic@gmail.com>
2024-05-29 10:43:39 +0200

    Added GET for a singular, randomly generated animal

---
+++ WebAPI.WebAPI/Animal.cs
@@ -0,0 +1,9 @@
+﻿namespace WebAPI.WebAPI
+{
+    public class Animal
+    {
+        public string Name { get; set; }
+        public string Color { get; set; }
+        public int Age { get; set; }
+    }
+}
---
+++ WebAPI.WebAPI/Controllers/AnimalController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebAPI.WebAPI.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class AnimalController : Controller
+    {
+        private List<string> Animals = new List<string> { "Pig", "Dog", "Cat", "Bird", "Rabbit" };
+        private List<string> Colors = new List<string> { "Red", "Blue", "Black", "Green", "Orange"};
+        [HttpGet(Name = "GetAnimal")]
+        public Animal Get() {
+            return new Animal
+            {
+                Name = Animals[Random.Shared.Next(Animals.Count)],
+                Age = Random.Shared.Next(12),
+                Color = Colors[Random.Shared.Next(Colors.Count)]
+            };
+        }
+           
+        
+    }
+}
